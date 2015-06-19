8d360bba2dab3abf53f35cb40b0bea589a92ed38
Jarrett Widman <jarrett@24m2.com>
2015-06-19 11:15:19 -0500

    enable more cloudfront configuration updates

--- tasks/services/cloudfront.js
+++ tasks/services/cloudfront.js
@@ -3,7 +3,7 @@ var AWS = require("aws-sdk"),
     async = require("async");
 
 module.exports = function(grunt) {
- 
+
   //cloudfront description
   var DESC = "grunt-aws's cloudfront";
 
@@ -17,10 +17,7 @@ module.exports = function(grunt) {
     var opts = this.options(DEFAULTS);
 
     if(_.isEmpty(opts.distributionId))
-      return grunt.log.ok("No DistributionId specified");    
-
-    if(_.isEmpty(opts.invalidations))
-      return grunt.log.ok("No invalidations specified");
+      return grunt.log.ok("No DistributionId specified");
 
     //mark as async
     var done = this.async();
@@ -34,12 +31,18 @@ module.exports = function(grunt) {
     //cloudfront client
     var cloudfront = new AWS.CloudFront();
 
-    //create records defined in opts.invalidations
-    createInvalidations(done);
+    var subtasks = [];
+    subtasks.push(createInvalidations);
+    subtasks.push(createUpdates);
+    async.series(subtasks, done);
 
     //------------------------------------------------
 
+    //create records defined in opts.invalidations
     function createInvalidations(callback) {
+      if(!opts.invalidations || !opts.invalidations.length)
+        return callback();
+
       var params = {
         DistributionId: opts.distributionId,
         InvalidationBatch: {
@@ -56,6 +59,47 @@ module.exports = function(grunt) {
         callback(err);
       });
     }
+
+    function createUpdates(callback) {
+      if(!opts.customErrorResponses && !opts.originPath && !opts.defaultRootObject)
+        return callback();
+
+      cloudfront.getDistribution({ Id: opts.distributionId }, function(err, res) {
+        if (err) {
+          console.log(err, err.stack);
+          return callback(err);
+        }
+
+        var params = {
+          Id: opts.distributionId,
+          DistributionConfig: res.Distribution.DistributionConfig,
+          IfMatch: res.ETag
+        };
+
+        if(opts.customErrorResponses){
+          params.DistributionConfig.CustomErrorResponses = {
+            Quantity: opts.customErrorResponses.length,
+            Items: opts.customErrorResponses
+          };
+        }
+
+        if(opts.originPath){
+          params.DistributionConfig.Origins.Items[ 0 ].OriginPath = opts.originPath;
+        }
+
+        if(opts.defaultRootObject){
+          params.DistributionConfig.DefaultRootObject = opts.defaultRootObject;
+        }
+
+        cloudfront.updateDistribution(params, function(err, data) {
+          if (err) console.log(err, err.stack);
+          else console.log(data);
+          callback(err);
+        });
+      });
+
+    }
+
   });
 
 
